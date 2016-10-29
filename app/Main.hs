e16c190057fff2b1e128b7dfc79f96b66f97596d
Sridhar Ratnakumar <happyandharmless@icloud.com>
2016-10-29 14:43:42 +0530

    Rename; this is now a script collection

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,6 @@ module Main where
 
 import System.IO
 import Control.Monad
-import Lib
 
 dataFile :: String
 dataFile = "movies-list.txt"
@@ -11,6 +10,6 @@ main :: IO ()
 main = do
   handle <- openFile dataFile ReadMode
   contents <- hGetContents handle
-  let output = parseMovie <$> lines contents
-  print output
+  -- let output = parseMovie <$> lines contents
+  -- print output
   hClose handle
