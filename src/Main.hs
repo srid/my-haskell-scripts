e2f7e6cab6e77d94fdd762f20644e382feada79b
Sridhar Ratnakumar <happyandharmless@icloud.com>
2016-10-29 14:56:32 +0530

    Cleanup

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,4 @@
 module Main where
 
-import System.IO
-import Control.Monad
-
-dataFile :: String
-dataFile = "movies-list.txt"
-
 main :: IO ()
-main = do
-  handle <- openFile dataFile ReadMode
-  contents <- hGetContents handle
-  -- let output = parseMovie <$> lines contents
-  -- print output
-  hClose handle
+main = putStrLn "Run the scripts under src/ directly!"
